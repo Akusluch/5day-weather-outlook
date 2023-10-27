dbd6e7b765ce0307c7325791efef21701442bd33
Alphonse Kusluch <alkusluch@gmail.com>
2023-10-27 19:05:08 -0400

    added improved fetch request for geocoding and fiveday utilizing lat and lon from first fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,29 @@
-var apikey = "0fd9a20b9ab89abff80b203942e7a06d"
-var geocoding = "http://api.openweathermap.org/geo/1.0/direct?q=city,us&appid=apikey"
-var fiveday = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=apikey"
-var city = "pittsburgh"
+var apikey = "0fd9a20b9ab89abff80b203942e7a06d";
 
-function getlatlon() {
-    fetch(geocoding)
-        .then(function (response) {
-        return response.json();
-        })
-        .then(function (data) {
-        console.log(data)
-        });
-}
+// function getlatlon() {
+//     var city = "pittsburgh";
+//     var geocodingurl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + ",us&appid=" + apikey;
+//     fetch(geocodingurl)
+//         .then(function (response) {
+//         return response.json();
+//         })
+//         .then(function (data) {
+//         console.log(data);
+//         var lat = data[0].lat
+//         var lon = data[0].lon
+//         return getfiveday(lat,lon)
+//         });
+// }
 
-getlatlon()
+// getlatlon()
+
+// function getfiveday(lat,lon) {
+//     var fiveday = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apikey;
+//     fetch(fiveday) 
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+// }
