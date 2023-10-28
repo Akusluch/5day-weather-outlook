7fdc61e2f728ddeb7549e98f2f679b7f84981af3
Alphonse Kusluch <alkusluch@gmail.com>
2023-10-27 20:18:42 -0400

    tried to add search bar and link functionality to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,32 @@
 var apikey = "0fd9a20b9ab89abff80b203942e7a06d";
+var citysearch = $('#citysearch');
+var searchbtn = $('#searchbtn');
 
-// function getlatlon() {
-//     var city = "pittsburgh";
-//     var geocodingurl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + ",us&appid=" + apikey;
-//     fetch(geocodingurl)
-//         .then(function (response) {
-//         return response.json();
-//         })
-//         .then(function (data) {
-//         console.log(data);
-//         var lat = data[0].lat
-//         var lon = data[0].lon
-//         return getfiveday(lat,lon)
-//         });
-// }
+searchbtn.on("click", getlatlon());
 
-// getlatlon()
+function getlatlon() {
+    var city = citysearch.textContent;
+    var geocodingurl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + ",us&appid=" + apikey;
+    fetch(geocodingurl)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+        console.log(data);
+        var lat = data[0].lat
+        var lon = data[0].lon
+        return getfiveday(lat,lon)
+        });
+}
 
-// function getfiveday(lat,lon) {
-//     var fiveday = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apikey;
-//     fetch(fiveday) 
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         })
-// }
+function getfiveday(lat,lon) {
+    var fiveday = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apikey;
+    fetch(fiveday) 
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
